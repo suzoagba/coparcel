908af1669111d1b708019527ffd0764fd10e76b6
suzoagba <samuzoagba@gmail.coma>
2023-11-11 22:41:12 +0200

    Contact and About Pages

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,22 +1,21 @@
-import Image from 'next/image'
 import Link from 'next/link'
 import Navbar from "../../components/Navbar"
-import Hero from "../../components/Hero"
 import Footer from '@/components/Footer'
+import AboutPage from '@/components/About'
 
 export default function About() {
   return (
     <div>
         <Navbar />
-        <Hero />
+        <AboutPage />
         <Footer />
-    <h1 className='text-red'>This is About Page</h1>
+    {/* <h1 className='text-red'>This is About Page</h1>
     <Link href={"/"}>
         Go back to Home Page
     </Link>
     <Link href={"/contact"}>
         Go to Contact Page
-    </Link>
+    </Link> */}
     </div>
     
   )
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,16 +1,18 @@
 import Link from 'next/link'
 import Navbar from "../../components/Navbar"
-export default function Home() {
+import Contact from '@/components/Contact'
+export default function Contact1() {
   return (
     <div>
         <Navbar />
-    <h1>Contact Page</h1>
+        <Contact />
+    {/* <h1>Contact Page</h1>
     <Link href={"/"}>
         Go back to home page
     </Link>
     <Link href={"/about"}>
         Go to About Aage
-    </Link>
+    </Link> */}
     </div>
     
   )
